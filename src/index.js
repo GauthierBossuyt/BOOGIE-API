2642c69807e7c1d9d2084ca7b56d8c4f60d6bb8a
Gauthier Bossuyt <55983896+GauthierBossuyt@users.noreply.github.com>
2022-06-08 16:36:42 +0200

    fix(port): bug from port env variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const HTTP = require("http");
 const EXPRESS = require("./server.js");
 const SOCKET = require("./socket.js");
-const PORT = process.env.DATABASE_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const SERVER = HTTP.createServer(EXPRESS);
 SOCKET.initialize(SERVER);
