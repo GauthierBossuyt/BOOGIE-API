13f3472375801b99065f74522d2632cda8658797
Gauthier Bossuyt <55983896+GauthierBossuyt@users.noreply.github.com>
2022-06-21 16:41:34 +0200

    fix(socket_url): socket cors blocking connection

--- src/socket.js
+++ src/socket.js
@@ -11,7 +11,7 @@ class Socket {
   initialize(server) {
     this.io = new Server(server, {
       cors: {
-        origin: `${process.env.SPOTIFY_REDIRECT_URI}`,
+        origin: `${process.env.SOCKET_URL}`,
       },
     });
 
